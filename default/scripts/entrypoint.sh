5842da53513b4294a45f984273d6f21e97d7dc04
Michael Skarbek <mskarbek@redhat.com>
2021-12-14 19:09:37 -0500

    fix CVE-2021-45046 and set reasonable default configuration

--- default/scripts/entrypoint.sh
+++ default/scripts/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-export HADOOP_HOME=/opt/hadoop-${HADOOP_VERSION}
 export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-${HADOOP_VERSION}.jar
-export METASTORE_OPTS="${METASTORE_OPTS} --hiveconf metastore.root.logger=INFO,console "
-export PATH=${METASTORE_HOME}/bin:$PATH
+export HIVE_OPTS="${HIVE_OPTS} --hiveconf metastore.root.logger=${HIVE_LOGLEVEL},console "
+export PATH=${METASTORE_HOME}/bin:${HADOOP_HOME}/bin:$PATH
 
 set +e
 if schematool -dbType postgres -info -verbose; then
--- docker-build-dev.sh
+++ docker-build-dev.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 IMAGE_TAG=$(${SCRIPT_DIR}/get_image_tag.sh)
 
 echo "Executing local hive-metastore docker image build..."
-docker build --target build \
+docker build \
        -t quay.io/cloudservices/ubi-hive:latest \
        -t quay.io/cloudservices/ubi-hive:${IMAGE_TAG} \
        -f "${SCRIPT_DIR}/Dockerfile" \
