b8dee862d753679eb5aacdb35442ef3c99a41a48
Timothy Regan <t_d_regan@yahoo.com>
2022-07-28 14:36:43 -0400

    Support for arbitrary user ids (#34)
    Co-authored-by: Shawn Zhu <shawn.zhu@lifeomic.com>

--- default/scripts/entrypoint.sh
+++ default/scripts/entrypoint.sh
@@ -6,6 +6,24 @@ export HADOOP_OPTS="${HADOOP_OPTS} ${VM_OPTIONS} ${GC_SETTINGS} ${JMX_OPTIONS}"
 export HIVE_OPTS="${HIVE_OPTS} --hiveconf metastore.root.logger=${HIVE_LOGLEVEL},console "
 export PATH=${METASTORE_HOME}/bin:${HADOOP_HOME}/bin:$PATH
 
+set -e
+# add UID to /etc/passwd if missing
+if ! whoami &> /dev/null; then
+    if test -w /etc/passwd || stat -c "%a" /etc/passwd | grep -qE '.[267].'; then
+        echo "Adding user ${USER_NAME:-hadoop} with current UID $(id -u) to /etc/passwd"
+        # Remove existing entry with user first.
+        # cannot use sed -i because we do not have permission to write new
+        # files into /etc
+        sed  "/${USER_NAME:-hadoop}:x/d" /etc/passwd > /tmp/passwd
+        # add our user with our current user ID into passwd
+        echo "${USER_NAME:-hadoop}:x:$(id -u):0:${USER_NAME:-hadoop} user:${HOME}:/sbin/nologin" >> /tmp/passwd
+        # overwrite existing contents with new contents (cannot replace the
+        # file due to permissions)
+        cat /tmp/passwd > /etc/passwd
+        rm /tmp/passwd
+    fi
+fi
+
 set +e
 if schematool -dbType postgres -info -verbose; then
     echo "Hive metastore schema verified."
